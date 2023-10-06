5d64e68ad2f71f521993f5a441c8c5f23561eb64
minsterboy <minsterboy@naver.com>
2023-10-06 09:43:00 +0900

    post 설정

--- testServer/urls.py
+++ testServer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView, PostCreateView
+from .views import PostListCreateView, PostDetailView, post_create
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('write/', PostCreateView.as_view(), name='post-create'),
+    path('write/', post_create.send, name='post_create'),
 ]
--- testServer/views.py
+++ testServer/views.py
@@ -1,6 +1,8 @@
+from django.utils import timezone
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+
 class PostListCreateView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -9,6 +11,12 @@ class PostDetailView(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostCreateView(generics.CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class post_create(Post):
+    def send(self,request):
+        if request.method == 'POST':
+            form = PostSerializer(request.POST)
+            if form.is_valid():
+                question = form.save(commit=False)
+                question.create_date = timezone.now()
+                question.save()
+
